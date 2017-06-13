2a7cac9efa4fdf41aee63b687223989de90aceb1
Carlos Gorrin <carlos.suarez@gtb.com>
2017-06-13 18:29:36 -0300

    hidden function

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,16 +2,16 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+	let input = document.getElementById('user-guess');
+	if(answer.value == '' || attempt.value == '') {
+		setHiddenFields();
+	}
 }
 
-//implement new functions here
-
-setHiddenFields(min, max) {
-		var min = 0000;
-		var max = 9999;
-		var x = Math.floor(Math.random() * (max - min)) + min;
-		// document.getElementById("dni").value = x;
-
-	};
+function setHiddenFields() {
+	aswer.value = Math.floor(Math.random() * 10000).toString();
+	while(aswer.value.length < 4) {
+		answer.value = "0" + answer.value;
+	}
+	attempt.value = "0";
+}
